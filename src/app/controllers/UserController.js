f0e1a4e8df251a7d3e511e47e1781351ce7c8b07
Luan Carlos <luancarlos@localhost.localdomain>
2020-01-19 11:11:39 -0300

    Created the mehotd to check if user exists

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,25 @@
+import User from '../models/User';
+
 class UserController {
   async index(req, res) {
-    return res.json({ message: 'Ok' });
+    const users = await User.findAll().then(user => user);
+    return res.json({ users });
+  }
+
+  async store(req, res) {
+    const storagedUsers = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (storagedUsers) {
+      return res.status(422).json({
+        error: 'User already exists',
+      });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.status(200).json();
   }
 }
 export default new UserController();
