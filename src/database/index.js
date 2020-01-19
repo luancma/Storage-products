6cf872e3f10ec3e9182d6492c77dc32be6ac24ca
Luan Carlos <luancarlos@localhost.localdomain>
2020-01-19 11:12:12 -0300

    Created the model loader

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 import Sequelize from 'sequelize';
 
-import Users from '../app/models/Users';
+import User from '../app/models/User';
 
 import databaseConfig from '../config/database';
 
-const models = [Users];
+const models = [User];
 class Database {
   constructor() {
     this.init();
